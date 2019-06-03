430b118838be791f44ceffb3da4f3ff1dc2e7a4d
Vicente <vicentemline@gmail.com>
2019-06-03 18:49:19 +0200

    Primer commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## Socket - Server
+
+Reconstruir módulos de Node
+```
+npm install 
+```
+
+Generar el DIST
+```
+tsc -w
+```
+
+Levantar el servidor - Sirve qualquiera de los dos
+```
+nodemon dist/
+node dist/
+```
---
+++ index.ts
@@ -0,0 +1,32 @@
+import Server from './class/server';
+import router from './routes/routes';
+import bodyParse from 'body-parser';
+import cors from 'cors';
+
+const server = new Server();
+
+//=================================================
+// BodyParse 
+//=================================================
+
+server.app.use( bodyParse.urlencoded({ extended: true }));
+server.app.use( bodyParse.json() );
+
+//=================================================
+// CORS 
+//=================================================
+
+server.app.use( cors({ origin:true, credentials:true }));
+
+//=================================================
+// Rutas Servicios
+//=================================================
+
+server.app.use('/', router);
+
+server.start( () => {
+    console.log(`Servidor corriendo en el puerto ${ server.port }`);
+})
+
+
+
